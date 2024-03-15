885a3dfd96e8625fa65c748093a58ac546375d45
rar34 <ra.cse34@gmail.com>
2024-03-16 01:45:47 +0600

    updated card

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,3 +1,4 @@
+
 import PropTypes from 'prop-types'
 import { IoMdTime } from "react-icons/io";
 import { MdOutlineLocalFireDepartment } from "react-icons/md";
@@ -7,7 +8,7 @@ import { MdOutlineLocalFireDepartment } from "react-icons/md";
 const Recipe = ({ recipe }) => {
     // console.log(recipe)
     const { recipe_name, recipe_image, short_description, ingredients, preparing_time, calories } = recipe;
-    console.log(ingredients);
+    // console.log(ingredients);
     return (
         <div>
             <div className="card bg-base-100 shadow-lg">
@@ -15,6 +16,14 @@ const Recipe = ({ recipe }) => {
                 <div className="card-body">
                     <h2 className="card-title">{recipe_name}</h2>
                     <p>{short_description}</p>
+                    <hr />
+                    <div>
+                        {
+                            ingredients.map((ingredient,idx) => <li key={idx}>{ingredient}</li> )
+                        }
+
+                    </div>
+                    <hr />
                     <div className='flex gap-6 mb-4'>
                         <span className='flex items-center gap-2'>{<IoMdTime/>} {preparing_time} min</span>
                         <span className='flex items-center gap-2'>{<MdOutlineLocalFireDepartment/>} {calories} Calories</span>
